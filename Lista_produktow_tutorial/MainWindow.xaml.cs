8a0c47da2a1caf1fd4bdf4e04cdaf5fc7ef11b60
Wirginia Tomczyk <virginiapyt@poczta.onet.pl>
2021-12-28 17:19:26 +0100

    Dodaj pliki projektów.

---
+++ Lista_produktow_tutorial/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lista_produktow_tutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> ListOfProduct { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListOfProduct = new ObservableCollection<Product>();
+            ListOfProduct.Add(new Product(1, "Kawa", (decimal)34.23, new DateTime(2022, 12, 4)));
+            ListOfProduct.Add(new Product(2, "mleko", (decimal)10.23, new DateTime(2022, 1, 4)));
+            ListOfProduct.Add(new Product(3, "herbata", (decimal)2, new DateTime(2022, 10, 22)));
+            DataContext = this;
+        }
+    }
+}
---
+++ Lista_produktow_tutorial/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_produktow_tutorial
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public Product(int id, string name, decimal price, DateTime expirationDate)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            ExpirationDate = expirationDate;
+        }
+    }
+}
